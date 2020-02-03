a41f129d05c4cacf0d89544a351f97b8849edb6a
Tanvigupta96 <guptatannu.34gmail.com>
2020-02-03 21:31:09 +0530

    added signout buttom

--- app/src/main/java/com/example/puchospeaklisten/DashBoardActivity.java
+++ app/src/main/java/com/example/puchospeaklisten/DashBoardActivity.java
@@ -1,10 +1,13 @@
 package com.example.puchospeaklisten;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import com.google.firebase.auth.FirebaseAuth;
 
@@ -19,14 +22,35 @@ public class DashBoardActivity extends AppCompatActivity {
 
         mAuth = FirebaseAuth.getInstance();
 
-        findViewById(R.id.buttonLogout).setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.logout).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mAuth.signOut();
+                AlertDialog.Builder builder1 = new AlertDialog.Builder(DashBoardActivity.this);
+                builder1.setMessage("Do You want to Logout of the app?");
+                builder1.setCancelable(true);
 
-                Intent intent = new Intent(DashBoardActivity.this, PhoneLoginActivity.class);
-                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                startActivity(intent);
+                builder1.setPositiveButton(
+                        "Yes",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                mAuth.signOut();
+                                Intent intent = new Intent(DashBoardActivity.this, PhoneLoginActivity.class);
+                                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                                startActivity(intent);
+                            }
+                        });
+
+                builder1.setNegativeButton(
+                        "No",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                dialog.cancel();
+                            }
+                        });
+
+                AlertDialog alert11 = builder1.create();
+                alert11.show();
+
             }
         });
     }
